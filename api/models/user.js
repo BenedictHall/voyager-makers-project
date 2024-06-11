374254bb6b90ec7a98b91e32870375aa652ae0a7
Benedict <BenedictH2468@gmail.com>
2024-06-11 15:24:21 +0100

    signup and login

--- api/models/user.js
+++ api/models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true },
+  firstname: { type: String, required: true, trim: true},
+  lastname: { type: String, required: true, trim: true},
+  username: {type: String, required: true, unique: true, trim: true},
+  email: { type: String, required: true, unique: true, trim: true },
   password: { type: String, required: true },
 });
 
--- frontend/src/pages/Signup/SignupPage.jsx
+++ frontend/src/pages/Signup/SignupPage.jsx
@@ -1,54 +1,136 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { isEmail, isStrongPassword } from "validator";
 import { signup } from "../../services/authentication";
 
 export const SignupPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [error, setError] = useState("");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await signup(email, password);
-      console.log("redirecting...:");
-      navigate("/login");
-    } catch (err) {
-      console.error(err);
-      navigate("/signup");
-    }
-  };
+    const navigate = useNavigate();
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+        if (firstname.length < 1) {
+            setError('Please enter your first name.');
+            return;
+        }
 
-  return (
-    <>
-      <h2>Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          id="email"
-          type="text"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          placeholder="Password"
-          id="password"
-          type="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <input role="submit-button" id="submit" type="submit" value="Submit" />
-      </form>
-    </>
-  );
+        if (lastname.length < 1) {
+            setError('Please enter your last name.');
+            return;
+        }
+        if (username.length < 5){
+            setError("Username must be at least 5 characters.");
+            return;
+        }
+
+        if (!isEmail(email)){
+            setError("Please enter a valid email address.");
+            return;
+        }
+
+        if (!isStrongPassword(password)){
+            setError("Password must be at least 8 characters, with at least one special character, one uppercase and one lower case.")
+            return; 
+        }
+        try {
+            await signup(email, password, username, firstname, lastname);
+            console.log("redirecting...:");
+            navigate("/login");
+        } catch (err) {
+            console.error(err);
+            console.log("Error: !!!!: ", err.message);
+            if (err.message === "Username already exists") {
+                setError("Username already exists. Please enter a valid username.");
+            }else if (err.message === "Email already exists") {
+                setError("Email already exists. Please enter a valid email address.");
+            } else {
+                setError("An error occurred. Please try again later.");
+            }
+            navigate("/signup");
+        }
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handleFirstnameChange = (event) => {
+        setFirstname(event.target.value);
+    };
+
+    const handleLastnameChange = (event) => {
+        setLastname(event.target.value);
+    };
+
+    return (
+        <>
+        <h2>Signup</h2>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="firstname">Firstname:</label>
+            <input
+                placeholder="Firstname"
+                id="firstname"
+                type="firstname"
+                value={firstname}
+                onChange={handleFirstnameChange}
+            />
+            <br />
+            <label htmlFor="lastname">Lastname:</label>
+            <input
+                placeholder="Lastname"
+                id="lastname"
+                type="lastname"
+                value={lastname}
+                onChange={handleLastnameChange}
+            />
+            <br />
+            <label htmlFor="username">Username:</label>
+            <input
+                placeholder="Username"
+                id="username"
+                type="username"
+                value={username}
+                onChange={handleUsernameChange}
+            />
+            <br />
+            <label htmlFor="email">Email:</label>
+            <input
+                id="email"
+                type="text"
+                value={email}
+                onChange={handleEmailChange}
+            />
+            <br />
+            <label htmlFor="password">Password:</label>
+            <input
+                placeholder="Password"
+                id="password"
+                type="password"
+                value={password}
+                onChange={handlePasswordChange}
+            />
+            <br />
+            {error && <p className="error">{error}</p>}
+            <input role="submit-button" id="submit" type="submit" value="Submit" />
+            </form>
+        <div>
+            <span>Already have an account? <a href="/login">Log in</a></span>
+        </div>
+        </>
+    );
 };
