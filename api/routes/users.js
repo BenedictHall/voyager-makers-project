e70ae8ba7ea3a788570e7e0ea9c264cfff40c855
Benedict <BenedictH2468@gmail.com>
2024-06-11 11:19:27 +0100

    setup

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,1 +1,10 @@
 
+const express = require("express");
+
+const UsersController = require("../controllers/users");
+
+const router = express.Router();
+
+router.post("/", UsersController.create);
+
+module.exports = router;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./App.css";
+import { HomePage } from "./pages/Home/HomePage";
+import { LoginPage } from "./pages/Login/LoginPage";
+import { SignupPage } from "./pages/Signup/SignupPage";
+
+// docs: https://reactrouter.com/en/main/start/overview
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
+
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
