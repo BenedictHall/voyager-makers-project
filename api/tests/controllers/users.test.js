9d3402d9aef2efcd9169a48e92a5b461d10e6a75
Andrew Zarei <andrew.zarei.09@gmail.com>
2024-06-11 17:39:18 +0100

    backend testing for user signup passing

--- api/tests/controllers/users.test.js
+++ api/tests/controllers/users.test.js
@@ -10,11 +10,16 @@ describe("/users", () => {
     await User.deleteMany({});
   });
 
-  describe("POST, when email and password are provided", () => {
+  describe("POST, when firstname, lastname, username, email and password are sent", () => {
     test("the response code is 201", async () => {
       const response = await request(app)
         .post("/users")
-        .send({ email: "poppy@email.com", password: "1234" });
+        .send({
+          firstname: "John",
+          lastname: "Smith",
+          username: "JSmith",
+          email: "johnsmith@example.com",
+          password: "Password1!",});
 
       expect(response.statusCode).toBe(201);
     });
@@ -22,11 +27,16 @@ describe("/users", () => {
     test("a user is created", async () => {
       await request(app)
         .post("/users")
-        .send({ email: "scarconstt@email.com", password: "1234" });
+        .send({
+          firstname: "John",
+          lastname: "Smith",
+          username: "JSmith",
+          email: "johnsmith@example.com",
+          password: "Password1!",});
 
       const users = await User.find();
       const newUser = users[users.length - 1];
-      expect(newUser.email).toEqual("scarconstt@email.com");
+      expect(newUser.email).toEqual("johnsmith@example.com");
     });
   });
 
@@ -34,13 +44,18 @@ describe("/users", () => {
     test("response code is 400", async () => {
       const response = await request(app)
         .post("/users")
-        .send({ email: "skye@email.com" });
+        .send({
+          firstname: "John",
+          lastname: "Smith",
+          username: "JSmith",
+          email: "johnsmith@example.com",
+        });
 
       expect(response.statusCode).toBe(400);
     });
 
-    test("does not create a user", async () => {
-      await request(app).post("/users").send({ email: "skye@email.com" });
+    test("does not create a user when missing password", async () => {
+      await request(app).post("/users").send({ firstname: "John", lastname: "Smith", username: "JSmith", email: "johnsmith@example.com"});
 
       const users = await User.find();
       expect(users.length).toEqual(0);
@@ -51,13 +66,18 @@ describe("/users", () => {
     test("response code is 400", async () => {
       const response = await request(app)
         .post("/users")
-        .send({ password: "1234" });
+        .send({
+          firstname: "John",
+          lastname: "Smith",
+          username: "JSmith",
+          password: "Password1!",
+        });
 
       expect(response.statusCode).toBe(400);
     });
 
-    test("does not create a user", async () => {
-      await request(app).post("/users").send({ password: "1234" });
+    test("does not create a user when email is missing", async () => {
+      await request(app).post("/users").send({ firstname: "John", lastname: "Smith", username: "JSmith", password: "Password1!"});
 
       const users = await User.find();
       expect(users.length).toEqual(0);
--- api/tests/models/user.test.js
+++ api/tests/models/user.test.js
@@ -6,37 +6,38 @@ describe("User model", () => {
     await User.deleteMany({});
   });
 
-  it("has an email address", () => {
+  it("formats correctly as a schema", () => {
     const user = new User({
-      email: "someone@example.com",
-      password: "password",
+      firstname: "John",
+      lastname: "Smith",
+      username: "JSmith",
+      email: "johnsmith@example.com",
+      password: "Password1!",
     });
-    expect(user.email).toEqual("someone@example.com");
+    expect(user.firstname).toEqual("John");
+    expect(user.lastname).toEqual("Smith");
+    expect(user.username).toEqual("JSmith");
+    expect(user.email).toEqual("johnsmith@example.com");
+    expect(user.password).toEqual("Password1!");
   });
 
-  it("has a password", () => {
-    const user = new User({
-      email: "someone@example.com",
-      password: "password",
-    });
-    expect(user.password).toEqual("password");
-  });
-
-  it("can list all users", async () => {
-    const users = await User.find();
-    expect(users).toEqual([]);
-  });
-
+  
   it("can save a user", async () => {
     const user = new User({
-      email: "someone@example.com",
-      password: "password",
+      firstname: "John",
+      lastname: "Smith",
+      username: "JSmith",
+      email: "johnsmith@example.com",
+      password: "Password1!",
     });
 
     await user.save();
     const users = await User.find();
 
-    expect(users[0].email).toEqual("someone@example.com");
-    expect(users[0].password).toEqual("password");
+    expect(users[0].email).toEqual("johnsmith@example.com");
+    expect(users[0].firstname).toEqual("John");
+    expect(users[0].lastname).toEqual("Smith");
+    expect(users[0].username).toEqual("JSmith");
+    expect(users[0].password).toEqual("Password1!");
   });
 });
