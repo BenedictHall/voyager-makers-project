0c23c8fb1be3173485c5d03c918736ffa7106403
Atansuyi Oluwatomi <tomisinatansuyi@gmail.com>
2024-06-14 14:55:13 +0100

    working single trip page without test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import { Route , Routes} from "react-router-dom";
-
+import { Route , Routes, Outlet} from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./pages/Home/HomePage";
 import { LoginPage } from "./pages/Login/LoginPage";
@@ -8,28 +7,36 @@ import { AddNewTrip } from "./pages/AddNewTrip/AddNewTrip";
 import { ShowAllTrips } from "./pages/Trips/tripsPage"
 
 import { Navbar } from './components/Navbar/navbar.jsx';
+import { SingleTripPage } from "./pages/Trips/singleTripPage.jsx";
+
 
 import { DashboardPage } from "./pages/Dashboard/DashboardPage";
 import { FlightTracker } from "./pages/Transport/FlightTracker.jsx";
+const AuthLayout = () => (
+  <>
+    <Navbar/>
+    <Outlet />
+  </>
+)
+
 
 const App = () => {
   return (
-    <>
-        <div className = "App">
-          <Navbar/>
-          <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/dashboard" element={<DashboardPage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/signup" element={<SignupPage/>}/>
-            <Route path="/trips" element={<ShowAllTrips/>}/>
-            <Route path="/trips/newtrip" element={<AddNewTrip/>}/>
-            <Route path="/flight" element={<FlightTracker/>}/>
-
-          </Routes>
-        </div>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/signup" element={<SignupPage/>}/>
+      <Route element={<AuthLayout />}>
+        <Route path="trips">
+          <Route path=":tripId" element={<SingleTripPage />} />
+          <Route path="newtrip" element={<AddNewTrip />} />
+          <Route index element={<ShowAllTrips />} />
+        </Route>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/flight" element={<FlightTracker/>}/>
+      </Route>
+    </Routes>
   );
-};
+}
 
 export default App;
