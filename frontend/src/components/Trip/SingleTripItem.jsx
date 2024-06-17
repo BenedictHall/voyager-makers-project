fb5369c72349fc9d347475fc6ff8283286e80b75
Atansuyi Oluwatomi <tomisinatansuyi@gmail.com>
2024-06-17 11:29:16 +0100

    single trip page test

--- frontend/src/components/Trip/SingleTripItem.jsx
+++ frontend/src/components/Trip/SingleTripItem.jsx
@@ -6,9 +6,9 @@ export default function SingleTripItem (props) {
 
     return(
         <div key={data._id}>
-            <h2>{data.location}</h2>
-            <p>{data.startDate}</p> 
-            <p>{data.endDate}</p>
+            <h2 data-testid="singleTripLocation">{data.location}</h2>
+            <p data-testid="singleTripStartDate">{data.startDate}</p> 
+            <p data-testid="singleTripEndDate">{data.endDate}</p>
         </div>
     );
 }
