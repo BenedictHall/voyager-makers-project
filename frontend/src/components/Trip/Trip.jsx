e2dbb60ac9d59435c25adb33087eb4b185fc8879
Aisha Thakker <aisha.thakker@outlook.com>
2024-06-20 12:50:58 +0100

    frontend styling

--- frontend/src/components/Trip/Trip.jsx
+++ frontend/src/components/Trip/Trip.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export const Trip = (props) => {
     const navigate = useNavigate()
@@ -11,11 +13,20 @@ export const Trip = (props) => {
     }
     
     return (
-        <div key={tripId} onClick={viewTrip}>
-            <h2>{props.trip.location}</h2>
-            <p>{props.trip.startDate} {props.trip.endDate}</p>
-
-        </div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="../../../../public/depositphotos_404572958-stock-photo-perfect-beach-view-summer-holiday.jpg" />
+            <Card.Body>
+                <Card.Title>
+                <div key={tripId}/>
+                    <h4>{props.trip.location}</h4>
+                </Card.Title>
+                    <Card.Text>
+                    <p>{props.trip.startDate} {props.trip.endDate}</p>
+                    </Card.Text>
+                    <Button variant="primary" onClick={viewTrip}>Trip details</Button>
+                </Card.Body>
+        </Card>
+        
     )
 
 };
